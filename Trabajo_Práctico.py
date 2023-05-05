14973ae622e7f1883b6a2a4d76b45cdc58b5f358
Joaquin Villalba <villalba.joaquin.2005@gmail.com>
2023-05-04 22:33:41 -0300

    Trabajo_definitivo.py

--- Trabajo_Práctico.py
+++ Trabajo_Práctico.py
@@ -1,38 +1,49 @@
-import getpass
+# import getpass
 
-Usuario_adm = "1"
-Contraseña_adm = "12"
+# Usuario_adm = "1"
+# Contraseña_adm = "12"
 
-Contador = 0
-Usuario = 0
-Contraseña = 0 
-while ((Usuario != Usuario_adm) and (Contraseña != Contraseña_adm)) and Contador < 3: 
-   Usuario = input("Escriba su usuario: ")
-   Contraseña = getpass.getpass("Escriba su contraseña: ")
-   Contador = Contador + 1
-   if Usuario == Usuario_adm and Contraseña == Contraseña_adm:
-      print ("Hola, has ingresado correctamente")
-   elif Contador < 3: 
-      print ("Compruebe si su Usuario o Contraseña estan correctos. Vuelva a intentarlo")
-   else:
-      print("Su numero de intentos ha finalizado")
+# Contador = 0
+# Usuario = 0
+# Contraseña = 0 
+# while ((Usuario != Usuario_adm) and (Contraseña != Contraseña_adm)) and Contador < 3: 
+#    Usuario = input("Escriba su usuario: ")
+#    Contraseña = getpass.getpass("Escriba su contraseña: ")
+#    Contador = Contador + 1
+#    if Usuario == Usuario_adm and Contraseña == Contraseña_adm:
+#       print ("Hola, has ingresado correctamente")
+#    elif Contador < 3: 
+#       print ("Compruebe si su Usuario o Contraseña estan correctos. Vuelva a intentarlo")
+#    else:
+#       print("Su numero de intentos ha finalizado")
 
+# Menú_Principal = None
 
-
-if Usuario == Usuario_adm and Contraseña == Contraseña_adm:
-   Menú_Principal = input ("""Elija una opción:
-                           1. Gestión de locales
-                           2. Crear cuentas de dueños de locales
-                           3. Aprobar / Denegarsolicitud de descuento
-                           4. Gestión de Novedades
-                           5. Reporte de utilización de descuentos
-                           0. Salir 
-                           """)
-   if Menú_Principal == "1":
-      Menú_Principal = input("""
-            a) Crear locales
-            b) Modificar local
-            c) Eliminar local
-            d) Volver 
-            """)
-      print (Menú_Principal)
+# while Menú_Principal != 0: 
+#    if Usuario == Usuario_adm and Contraseña == Contraseña_adm:
+#       Menú_Principal = input ("""Elija una opción:
+#                            1. Gestión de locales
+#                            2. Crear cuentas de dueños de locales
+#                            3. Aprobar / Denegarsolicitud de descuento
+#                            4. Gestión de Novedades
+#                            5. Reporte de utilización de descuentos
+#                            0. Salir 
+#                            """)
+#       if Menú_Principal == "1":
+#          Menú_Principal = input("""
+#                            a. Crear locales
+#                            b. Modificar local
+#                            c. Eliminar local
+#                            d. Volver 
+#                            """)
+#          if Menú_Principal == "d":
+               
+#       if Menú_Principal == "a":
+#          Menú_Principal = input ("""
+#                            a. Nombre del Local:  
+#                            b. Ubicación de Local: 
+#                            c. Rubro del Local: 
+#                            """)
+#       elif Menú_Principal == "b" or "c":
+#          print ("En construcción...")
+      
